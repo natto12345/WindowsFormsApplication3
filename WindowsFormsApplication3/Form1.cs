1ed48cde842f0a9d7675a4e381c48ef1f219db6f
natto12345 <natthakit.chan@kkumail.com>
2016-12-06 00:27:27 +0700

    tv online

---
+++ WindowsFormsApplication3/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace WindowsFormsApplication3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Process.Start("http://www.adintrend.com/hd/ch3");
+        }
+
+        private void button22_Click(object sender, EventArgs e)
+        {
+            Process.Start("http://www.adintrend.com/hd/ch27");
+        }
+
+        private void button21_Click(object sender, EventArgs e)
+        {
+            Process.Start("http://www.adintrend.com/hd/ch31");
+        }
+
+        private void button20_Click(object sender, EventArgs e)
+        {
+            Process.Start("http://www.adintrend.com/hd/ch34");
+        }
+
+        private void button19_Click(object sender, EventArgs e)
+        {
+            Process.Start("http://www.adintrend.com/hd/ch23");
+        }
+
+        private void button18_Click(object sender, EventArgs e)
+        {
+            Process.Start("http://www.adintrend.com/hd/ch38");
+        }
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+            Process.Start("http://www.adintrend.com/hd/ch30");
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            Process.Start("http://www.adintrend.com/hd/ch39");
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            Process.Start("http://www.adintrend.com/hd/ch26");
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            Process.Start("http://www.adintrend.com/hd/ch0");
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            Process.Start("http://www.adintrend.com/hd/ch25");
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            Process.Start("http://www.adintrend.com/hd/ch35");
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            Process.Start("http://www.adintrend.com/hd/ch36");
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            Process.Start("http://www.adintrend.com/hd/ch33");
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            Process.Start("http://www.adintrend.com/hd/ch22");
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            Process.Start("http://www.adintrend.com/hd/ch24");
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            Process.Start("http://www.adintrend.com/hd/ch1");
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Process.Start("http://www.adintrend.com/hd/ch32");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Process.Start("http://www.adintrend.com/hd/ch37");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Process.Start("http://www.adintrend.com/hd/ch11");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Process.Start("http://www.adintrend.com/hd/ch9");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Process.Start("http://www.adintrend.com/hd/ch5");
+        }
+
+        private void button23_Click(object sender, EventArgs e)
+        {
+            Process.Start("http://www.adintrend.com/hd/ch28");
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
